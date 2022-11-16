e6afa8da44fcb604a91df518640844412db333ec
Zahra Dirie <114920272+zahrodirie@users.noreply.github.com>
2022-11-16 00:43:17 -0800

    Delete Lectures directory

--- Lectures/Day1/Example1.java
+++
@@ -1,8 +0,0 @@
-package Example 1;
-public class Example 1 {
- public static void main(String[] args) {
-  System.out.println("Hellow Filan Tech Team My Name Is Zahro Ahmed Dirie Today I will Started Filan Botcham And i learned Data Structure & Algorithms with Java in 30 Days");
-    
-    }
-    
-}
